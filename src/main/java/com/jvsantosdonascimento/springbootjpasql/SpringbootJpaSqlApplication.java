ab010981f755327b1bfc31e9d1a43507632582ea
jvictornascimento <victojoao789@gmail.com>
2025-08-27 22:02:25 -0300

    Started project

---
+++ src/main/java/com/jvsantosdonascimento/springbootjpasql/SpringbootJpaSqlApplication.java
@@ -0,0 +1,13 @@
+package com.jvsantosdonascimento.springbootjpasql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaSqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jvsantosdonascimento/springbootjpasql/SpringbootJpaSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvsantosdonascimento.springbootjpasql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
